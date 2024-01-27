7b687a1909875a78013d390149c1f769846d21bf
Irfanlam123 <irfanalam010202@gmail.com>
2024-01-28 00:35:44 +0530

    change in home

--- src/container/home/home.jsx
+++ src/container/home/home.jsx
@@ -3,11 +3,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-import Kaddu from '../../assets/kaddu.jpg'
+import Kaddu from "../../assets/kaddu.jpg";
 import Head from "./components/head";
 import Card from "./components/card";
-
-
 
 const Home = () => {
   const settings = {
@@ -16,7 +14,7 @@ const Home = () => {
     speed: 200,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:true,
+    autoplay: true,
   };
 
   return (
@@ -24,7 +22,6 @@ const Home = () => {
       <Slider {...settings}>
         <div className="">
           <img className="h-96 w-full" src={Kaddu} alt="img" />
-         
         </div>
         <div>
           <img className="h-96 w-full" src={Kaddu} alt="img" />
@@ -34,19 +31,19 @@ const Home = () => {
         </div>
       </Slider>
       <div className=" mt-1 relative  flex justify-center  text-2xl text-white font-bold">
-        <h1 className="absolute bottom-80 font-bold text-6xl ">ORDER FOOD DELIVERY</h1>
-        <p className="absolute bottom-72 font-bold text-sm">FROM YOUR FAVORITE RESTURANTS <button className="px-5 py-1 bg-yellow-400 rounded-full">Menu</button></p>
-       
+        <h1 className="absolute bottom-80 font-bold text-6xl ">
+          ORDER FOOD DELIVERY
+        </h1>
+        <p className="absolute bottom-72 font-bold text-sm">
+          FROM YOUR FAVORITE RESTURANTS{" "}
+          <button className="px-5 py-1 bg-yellow-400 rounded-full">Menu</button>
+        </p>
       </div>
-      <Head/>
+      <Head />
       <div className="flex container mx-auto p-4">
-      <Card title="Card 1" content="This is the content of Card 1." />
-     
-
+        <Card title="Card 2" content="This is the content of Card 1." />
+      </div>
     </div>
-
-    </div>
-    
   );
 };
 
