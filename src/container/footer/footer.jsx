527a16d4495a93db1fff379c2827b18c75a328ae
Irfanlam123 <irfanalam010202@gmail.com>
2024-01-29 23:36:59 +0530

    nothing

--- src/container/footer/footer.jsx
+++ src/container/footer/footer.jsx
@@ -10,11 +10,14 @@ const Footer = () => {
             Grow Your <br />
             Resturant <br />
             Business With Fodie
-          </h2><br />
+          </h2>
+          <br />
           <Link to="/menu">
-          <button className="bg-yellow-300 text-white mt-20 rounded-full px-5 py-1">Get Started</button>
-         
-          </Link> <div>
+            <button className="bg-yellow-300 text-white mt-20 rounded-full px-5 py-1">
+              Get Started
+            </button>
+          </Link>{" "}
+          <div>
             <h2 className="text-white text-lg p-7">Connect</h2>
             <div className="p-7  ">
               <ul className="text-white text-sm space-y-2 ">
@@ -35,10 +38,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className="ml-[90px] pt-4 text-white text-sm space-y-2  py-12">
-            <p>Privacy Policy</p>
-            <p>Terms and Conditions</p>
-            <p>2024,Fodie Dish.</p>
+        <div className="ml-[90px] pt-4 text-white text-sm space-y-2  py-20">
+          <p>Privacy Policy</p>
+          <p>Terms and Conditions</p>
+          <p>2024,Fodie Dish.</p>
         </div>
       </div>
     </div>
