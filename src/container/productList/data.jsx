2eb5674f6f8282f6aaaa4591448def1aa6e3a68c
Irfanlam123 <irfanalam010202@gmail.com>
2024-02-01 00:27:28 +0530

    product

---
+++ src/container/productList/data.jsx
@@ -0,0 +1,63 @@
+const items=[
+    {
+       id:1 ,
+       name:"chicken roll",
+       price:120,
+       img:{roll},
+    },
+    {
+        id:2 ,
+        name:"egg roll",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:3 ,
+        name:"burger",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:4,
+        name:"pizza",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:5 ,
+        name:"omlet",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:6,
+        name:"sushi",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:7,
+        name:"drink",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:8,
+        name:"maggi",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:9,
+        name:"murga bujiya",
+        price:120,
+        img:{roll},
+     },
+     {
+        id:10 ,
+        name:"papad",
+        price:120,
+        img:{roll},
+     },
+   
+]
